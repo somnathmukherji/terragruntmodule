539c121ffd632bf840361b1b68df93c3d538a8b5
somnathmukherji <99045348+somnathmukherji@users.noreply.github.com>
2023-06-07 14:44:06 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 # Input Variables
 # AWS Region
-variable "aws_region" {
-  description = "Region in which AWS Resources to be created"
-  type = string
-  default = "ap-south-1"
-}
+#variable "aws_region" {
+ # description = "Region in which AWS Resources to be created"
+  #type = string
+  #default = "ap-south-1"
+#}
 
 variable "instance_type" {
   description = "EC2 instance type"
