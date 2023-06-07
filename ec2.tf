d353199042c0e966cd6851ed509e448ee4db7417
somnathmukherji <99045348+somnathmukherji@users.noreply.github.com>
2023-06-07 15:21:25 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "NAT_INSTANCE" {
   ami = "${data.aws_ami.NAT_INSTANCE.id}"
   instance_type = "${var.instance_type}"
-  subnet_id = "{subnet-070afe3fd429c32ae}"
+  subnet_id = "subnet-070afe3fd429c32ae"
   vpc_security_group_ids = ["sg-025c8313b52a31689"]
   root_block_device {
     volume_type = "standard"
